b0d78a0c9cd6444c6c5b66cb69dbe9763cd91be6
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-05-05 16:08:00 -0400

    done

--- index.js
+++ index.js
@@ -35,6 +35,62 @@ server.use(express.json())
           .json({ errormessage: "The user with the specified ID does not exist." });
   });
 
+  server.post("/api/users", (req, res) => {
+    const user = req.body;
+    if (user.name === "" || user.bio === "") {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+      users.push(user);
+      res.status(201).json(user);
+    }
+    if (!user) {
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database",
+      });
+    }
+  });
+
+  server.delete('/api/users/:id', (req, res) => {
+    const delId = req.params.id;
+    let user = users.filter(user => {
+        return user.id == delId;
+    })[0];
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    res.status(500).json({
+        errorMessage: "The user with the specified ID does not exist."});
+});
+
+
+server.put("/api/users/:id", (req, res) => {
+	const userUpdate = req.body;
+	const id = req.params.id;
+
+	if (!userUpdate.name || !userUpdate.bio) {
+		res.status(400).json({
+         errorMessage: "Please provide name and bio for the user." });
+	} else {
+		const user = users.find((user) => user.id == id);
+		if (user) {
+			users = users.map((user) => {
+				return user.id == id ? { ...userUpdate, id } : user;
+			});
+			const updateUser = users.find((user) => user.id == id);
+			if (updateUser) {
+                res.json(users);
+            } else {
+                res.status(500).json({
+                 errorMessage: "The information could not be modified." });
+            }	 
+		} else {
+			res.status(404).json({
+             errormessage: "The user with the specified ID does not exist." });
+		}
+	}
+});
+
   const port = 5000;
   server.listen(port, () =>
     console.log(`api running on port ${port}`)
