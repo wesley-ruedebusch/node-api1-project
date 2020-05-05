b7b1bd7409cd5613f00d3e18634d555898769acb
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-05-05 15:54:45 -0400

    push issues

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+
+const server = express();
+
+let users = [
+    {
+      id: 1,
+      name: "Wesley Ruedebusch",
+      bio: 'My computer is being amazing. Sarcasm.'
+    },
+  ];
+
+
+server.use(express.json())
+
+
+  
+  server.get("/api/users", (req, res) => {
+    users
+      ? res.status(200).json(users)
+      : res.status(500).json({
+          errorMessage: "The users information could not be retrieved.",
+        });
+  });
+  
+  server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+  
+    const user = users.find((e) => e.id == id);
+  
+    user
+      ? res.json(user)
+      : res
+          .status(404)
+          .json({ errormessage: "The user with the specified ID does not exist." });
+  });
